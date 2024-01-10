5a019b74465c491a87798994df5eeab52b3e0d66
Joshua Lara <joshualara0910@gmail.com>
2024-01-10 10:09:19 -0700

    if its not numbers

---
+++ day3.js
@@ -0,0 +1,18 @@
+// Given the variables num1 and num2, write some code to evaluate the following:
+// If num1 is less than num2, print to the console, "<num1> is less than <num2>"
+// If num1 is greater than num2, print to the console, "<num1> is greater than <num2>"
+// If num1 is equal to num2, print to the console, "These numbers are equal"
+// Bonus: If either num1 or num2 is not a valid Number data type, print to the console, "Whoa, those aren't numbers!"
+
+const num1 = 'sean';
+const num2 = '300';
+
+if (isNaN(num1) || isNaN(num2)) {
+    console.log(`Whoa, those aren't numbers!`)
+} else if (num1 < num2) {
+    console.log('<num1> is less than <num2>')
+} else if (num2 < num1) {
+    console.log('<num1> is greater than <num2>')
+} else if (num2 === num1) {
+    console.log("These numbers are equal")
+} 
