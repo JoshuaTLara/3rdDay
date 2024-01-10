cc0ed975d341d0c8f4d97ce8de0583a4a9d1fd3e
Joshua Lara <joshualara0910@gmail.com>
2024-01-10 14:00:06 -0700

    end of lecture code day 3

--- day3.js
+++ day3.js
@@ -32,10 +32,53 @@
 
 // console.log(students)
 
-const perfectTemp = 125
-let teaTemp = 130
+// const perfectTemp = 125
+// let teaTemp = 130
 
-while (teaTemp > perfectTemp) {
-    teaTemp -= 1;
-    console.log(`tea temperature is now: ${teaTemp}`);
-}
+// while (teaTemp > perfectTemp) {
+//     teaTemp -= 1;
+//     console.log(`tea temperature is now: ${teaTemp}`);
+// }
+// const students = ['Alice', 'Bob', 'Charlie', 'Diana'];
+
+// // This is the use of a loop and index to find something within your array specifically while loop
+
+// let i = 0; // i stands for index
+// while (i < students.length) {
+//   console.log(students[i]);
+
+//   if (students[i] === 'Bob') {
+//     console.log('Hi Bob!');
+//     break; // we found Bob, no need to loop any further!
+//   }
+//   i += 1;
+// }
+
+//this is a "for" loop
+
+// Count by 2: 0, 2, 4, 6, 8
+// for (let n = 0; n < 10; n += 2) {
+//     console.log(n);
+//   }
+  
+//   // Countdown loop
+//   for (let n = 10; n > 0; n -= 1) {
+//     console.log(n);
+//   }
+//   console.log('Blast off!');
+
+
+  // ((start) let i = 0; (stop) i < meals.length; (step) i += 1) this is the order of a "for loop"
+
+  const grid = [
+    ['A', 'B', 'C'],
+    ['D', 'E', 'F'],
+    ['G', 'H', 'I'],
+  ];
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+        console.log(grid[i][j]);
+    }
+  }
+
